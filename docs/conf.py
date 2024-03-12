62cb23fe1b7e026c311695723b058b3c2e42792c
wpk <william.krekelberg@nist.gov>
2024-03-12 17:04:44 -0400

    chore: update cruft

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,8 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel",
-    "IPython.sphinxext.ipython_directive",
-    "IPython.sphinxext.ipython_console_highlighting",
+    # "IPython.sphinxext.ipython_directive",
+    # "IPython.sphinxext.ipython_console_highlighting",
     # "nbsphinx",
     # - easier external links
     # "sphinx.ext.extlinks",
@@ -61,7 +61,8 @@ extensions = [
     # - pretty things up?
     # "sphinx_design"
     # - myst stuff
-    "myst_nb",
+    # "myst_nb",
+    "myst_parser",
     "sphinx_click",
 ]
 
